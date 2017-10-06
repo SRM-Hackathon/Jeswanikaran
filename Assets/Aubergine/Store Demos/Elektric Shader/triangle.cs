58425e4201cf3ae5216ad3c3ebc591f502e426fc
barnwal-paras <parasbarnwal06@gmail.com>
2017-10-06 23:40:11 +0530

    Commit karan's_work

---
+++ Assets/Aubergine/Store Demos/Elektric Shader/triangle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triangle : MonoBehaviour {
+
+	public GameObject line1,line2,line3;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (linere2.hg) {
+			line1.SetActive (true);
+			line2.SetActive (true);
+			line3.SetActive (true);
+
+
+		} else {
+
+			line1.SetActive (false);
+			line2.SetActive (false);
+			line3.SetActive (false);
+		}
+	}
+}
