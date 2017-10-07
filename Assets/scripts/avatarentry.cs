f4caebd7c77ad530411c9d8c21a53ceed4ac5b82
barnwal-paras <parasbarnwal06@gmail.com>
2017-10-07 06:49:56 +0530

    Effects integration

---
+++ Assets/scripts/avatarentry.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class avatarentry : MonoBehaviour {
+	
+	void OnEnable(){
+
+		gameObject.transform.localScale = new Vector3 (0, 0, 0);
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.localScale.x < 1.09) {
+			gameObject.transform.transform.localScale = gameObject.transform.transform.localScale + new Vector3 (0.01f, 0.01f, 0.01f);
+
+		}
+		if (gameObject.transform.rotation.eulerAngles.y < 180) {
+
+			gameObject.transform.Rotate (new Vector3 (0, 1f, 0));
+		} else {
+			gameObject.transform.rotation=Quaternion.Euler(0, 180, 0);
+		}
+	}
+}
